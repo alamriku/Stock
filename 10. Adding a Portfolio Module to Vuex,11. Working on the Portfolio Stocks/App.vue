e6b4de7b769f03a61d0656f706c0c524011a9dac
Riku <alamriku069@gmail.com>
2020-04-24 01:40:29 +0600

    10. Adding a Portfolio Module to Vuex,11. Working on the Portfolio Stocks

---
+++ 10. Adding a Portfolio Module to Vuex,11. Working on the Portfolio Stocks/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+      <app-header></app-header>
+    <div class="row mt-5">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  components: {
+    appHeader: Header
+  },
+  created () {
+    // this is for initilize the app initstocks is on module/stocks.js file
+    this.$store.dispatch('initStocks')
+  }
+}
+</script>
+
+<style scoped>
+ body{
+   padding: 30px;
+
+ }
+</style>
---
+++ 10. Adding a Portfolio Module to Vuex,11. Working on the Portfolio Stocks/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  {id: 1, name: 'BMW', price: 110},
+  {id: 2, name: 'google', price: 120},
+  {id: 3, name: 'Apple', price: 1530},
+  {id: 4, name: 'Twitter', price: 150}
+]
